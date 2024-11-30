49fbb85cb3ac27bcd8cda8fac912ca378e2eefd0
RaihanNagata <122301807+RaihanNagata@users.noreply.github.com>
2024-11-30 20:07:06 +0700

    Delete phpmvc directory

--- phpmvc/app/views/about/index.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-    <img src="<?= BASEURL; ?>/img/Raihan.jpg" alt="Raihan Nagata" width="200" class="rounded-circle shadow">
-    <h1 class="mt-4">About Me</h1>
-    <p> Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
-</div>
